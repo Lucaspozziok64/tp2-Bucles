22802747921dccc3e2d8c65bd1b24a71e3b9cac5
Lucasfi <lukafigueroa64@gmail.com>
2025-04-18 22:47:38 -0300

    agregue estructura do while para el programa ejercicio-2 y comente

--- js/ejercicio-2.js
+++ js/ejercicio-2.js
@@ -10,3 +10,36 @@ Luego mostrar la calificación en un alert según los siguientes  rangos de nota
 Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
 */
 
+do {
+    let nota = parseInt(prompt("Ingrese su nota por favor de: (0-10)"));
+
+    if (isNaN(nota)){
+        alert("Introduce un numero valido");
+        continue;
+    }
+
+    if (nota < 0 || nota > 10) {
+        alert("Numero erróneo");
+        continue;
+    }
+
+    if (nota >= 0 && nota <= 2){
+        alert("Muy deficiente");
+        break;
+    } else if (nota >= 3 && nota <= 4){
+        alert("Insuficiente");
+        break;
+    } else if (nota >= 5 && nota <= 6){
+        alert("Suficiente");
+        break;
+    } else if (nota == 7){
+        alert("Bien");
+        break;
+    } else if (nota >= 8 && nota <= 9){
+        alert("Notable");
+        break;
+    } else if (nota == 10){
+        alert("Sobresaliente");
+        break;
+    }
+} while (true);
