1230535d9146701b1e1d4260288dbdb888db9b12
Lucasfi <lukafigueroa64@gmail.com>
2025-04-18 21:29:48 -0300

    agregue estructura while dentro de else para finalizar el programa

--- js/ejercicio-1.js
+++ js/ejercicio-1.js
@@ -6,4 +6,14 @@ let edad = parseInt(prompt("Ingrese su edad por favor"));
 
 if (isNaN(edad) || edad <=0 || edad >= 95) {
     document.writeln("<h1>No es un numero o la edad ingresada no esta en rango valido para conducir</h1>");
+} else {
+    while(true) {
+        if (edad >= 18) {
+            document.writeln("<h1>Usted puede conducir</h1>");
+            break;
+        } else {
+            document.writeln("<h1>Usted Aún no puede conducir</h1>");
+            break;
+        }
+    }
 }
