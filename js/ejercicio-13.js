6a1b01db5e7b87bdd33107eae345dba666c5e30f
Lucasfi <lukafigueroa64@gmail.com>
2025-04-20 20:42:55 -0300

    agregue un prompt y un metodo para convertir texto a mayusculas en ejercicio-13

--- js/ejercicio-13.js
+++ js/ejercicio-13.js
@@ -1,2 +1,7 @@
 // Realiza un script que pida un texto y lo muestre en mayúsculas.
 
+let texto = prompt("Introduce un texto");
+
+const textoMayuscula = texto.toUpperCase();
+
+document.writeln(textoMayuscula);
