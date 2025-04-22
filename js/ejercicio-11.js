fb48feb09d967e0dfc19d443d803133ae5e6f04b
Lucasfi <lukafigueroa64@gmail.com>
2025-04-22 15:32:30 -0300

    creamos el archivo ejercicio11 y agregamos variables, prompt para edad y nombres

---
+++ js/ejercicio-11.js
@@ -0,0 +1,11 @@
+/*
+Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. *
+*/
+
+let edad1 = parseInt(prompt("Introduce la edad de la 1era persona"));
+let edad2 = parseInt(prompt("Introduce la edad de la 2da persona"));
+let edad3 = parseInt(prompt("Introduce la edad de la 3era persona"));
+
+let nombre1 = prompt("Introduce el nombre de la 1era persona");
+let nombre2 = prompt("Introduce el nombre de la 2da persona");
+let nombre3 = prompt("Introduce el nombre de la 3era persona");
