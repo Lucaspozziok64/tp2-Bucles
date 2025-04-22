1bf3de454a9d3d7a7e6a3ac93cea001cdc423961
Lucasfi <lukafigueroa64@gmail.com>
2025-04-22 15:33:51 -0300

    agregamos condiciones if- else-if para determinar mayor y menor edad

--- js/ejercicio-11.js
+++ js/ejercicio-11.js
@@ -9,3 +9,13 @@ let edad3 = parseInt(prompt("Introduce la edad de la 3era persona"));
 let nombre1 = prompt("Introduce el nombre de la 1era persona");
 let nombre2 = prompt("Introduce el nombre de la 2da persona");
 let nombre3 = prompt("Introduce el nombre de la 3era persona");
+
+if (edad1 > edad2 && edad1 > edad3) {
+    alert(nombre1 + " es el mayor");
+} else if (edad2 > edad1 && edad2 > edad3) {
+    alert(nombre2 + " es el mayor");    
+} else if (edad3 > edad1 && edad3 > edad2) {
+    alert(nombre3 + " es el mayor");
+} else {
+    alert("Hay dos o más personas con la misma edad");
+}
