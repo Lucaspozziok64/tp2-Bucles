ae95f4322b848f3ba5983a5ac716c64b41f005cc
Lucasfi <lukafigueroa64@gmail.com>
2025-04-23 14:29:14 -0300

    cambie las variables a constantes

--- js/ejercicio-11.js
+++ js/ejercicio-11.js
@@ -2,13 +2,13 @@
 Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. *
 */
 
-let edad1 = parseInt(prompt("Introduce la edad de la 1era persona"));
-let edad2 = parseInt(prompt("Introduce la edad de la 2da persona"));
-let edad3 = parseInt(prompt("Introduce la edad de la 3era persona"));
+const edad1 = parseInt(prompt("Introduce la edad de la 1era persona"));
+const edad2 = parseInt(prompt("Introduce la edad de la 2da persona"));
+const edad3 = parseInt(prompt("Introduce la edad de la 3era persona"));
 
-let nombre1 = prompt("Introduce el nombre de la 1era persona");
-let nombre2 = prompt("Introduce el nombre de la 2da persona");
-let nombre3 = prompt("Introduce el nombre de la 3era persona");
+const nombre1 = prompt("Introduce el nombre de la 1era persona");
+const nombre2 = prompt("Introduce el nombre de la 2da persona");
+const nombre3 = prompt("Introduce el nombre de la 3era persona");
 
 if (edad1 > edad2 && edad1 > edad3) {
     alert(nombre1 + " es el mayor");
