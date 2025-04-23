1042828ae9225677454b37182ae46ab35f95c0ec
Lucasfi <lukafigueroa64@gmail.com>
2025-04-23 15:02:19 -0300

    agregamos condiciones para mostrar si es multiplo de..

--- js/ejercicio-9.js
+++ js/ejercicio-9.js
@@ -16,5 +16,18 @@ Crea un script que escriba los números del 1 al 500, que indique cuáles son m�
 */
 
 for (let i = 1; i <= 500; i++) {
+
+    if (i % 4 == 0) {
+        i += " (Múltiplo de 4)";
+    }
+
+    if (i % 9 == 0) {
+        i += " (Múltiplo de 9)";
+    }
+
+    if (i % 5 == 0) {
+        i += "<br>————————————————————-<br>";
+    }
+
     document.writeln(i + "<br>");
 }
